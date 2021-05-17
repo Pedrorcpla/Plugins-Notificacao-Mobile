980addd201152d08a83f3dbc7883e9a9ec22b65f
Pedrorcpla <pedrorcpla@gmail.com>
2021-05-18 00:14:51 +0900

    Leitor de código QR e de barras

--- www/components/Plugins.js
+++ www/components/Plugins.js
@@ -69,3 +69,33 @@ $(document).on('click','#camera',function(){
         alert('Failed because: ' + message);
     }
 });
+
+$(document).on('click','#codigo',function(){
+  cordova.plugins.barcodeScanner.scan(
+      function (result) {
+        if(result.text != ""){
+          alert("Resultado: " + result.text + "\n" +
+                "Formato: " + result.format + "\n");
+        }
+        else{
+          alert("Nenhum código detectado!");
+        }
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: false, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Mire a câmera no código desejado", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "portrait|landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+    );
+});
